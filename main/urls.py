59dd3214adc9e2c2395ddf20a4e6ef327bc76d35
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2024-09-04 12:15:42 +0100

    update deps

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<token>[a-z0-9]{10})/$', views.receiver, name='receiver'),
-    url(r'^logout/?$', views.logout_user, name='logout'),
-    url(r'^about/?$', views.about, name='about'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^(?P<token>[a-z0-9]{10})/$', views.receiver, name='receiver'),
+    re_path(r'^logout/?$', views.logout_user, name='logout'),
+    re_path(r'^about/?$', views.about, name='about'),
 ]
